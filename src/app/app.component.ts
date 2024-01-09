1bc51d4870435c9bf056a6f185a99c433e457fc0
Stefan Pfleger <stefan.pfleger@rocketmail.com>
2024-01-09 14:25:04 +0100

    create similar component login window

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { LoginScreenComponent } from './login-screen/login-screen.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavigationBarComponent, LoginScreenComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
